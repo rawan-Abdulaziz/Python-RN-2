38436e1cba78688a0d3c7a1026c5f7a19f48c9f7
rawan-Abdulaziz <75187944+rawan-Abdulaziz@users.noreply.github.com>
2021-04-23 00:53:13 +0200

    Add files via upload

---
+++ task_scheduler.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+cd /home/rawan/Desktop/ITI_Python_for_Data_Management/Task2/
+/home/rawan/anaconda3/bin/python3 Task_2.py /home/rawan/Desktop/ITI_Python_for_Data_Management/Task2/ > execution_logger.log
